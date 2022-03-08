c93044ea6814efd784a55f3f3bad17c84e687629
tijjvni <tijjvni@gmail.com>
2022-03-08 19:08:01 +0000

    chnging wallet styles

--- pages/wallet.js
+++ pages/wallet.js
@@ -28,10 +28,10 @@ export default function Home() {
                 </div>
 
                 <div className="flex">
-                    <div className="flex-1 justify-center rounded-lg text-sm mb-2" role="group">
-                        <button className="bg-gray-500 text-white hover:bg-gray-400 rounded-l-lg px-4 py-2 mx-0 outline-none focus:shadow-outline">First</button>
-                        <button className="bg-gray-500 text-white hover:bg-gray-400  px-4 py-2 mx-0 outline-none focus:shadow-outline">Second</button>
-                        <button className="bg-gray-500 text-white hover:bg-gray-400 rounded-r-lg px-4 py-2 mx-0 outline-none focus:shadow-outline">Third</button>
+                    <div className="flex-1 justify-center rounded-lg w-full text-sm mb-2" role="group">
+                        <button className="bg-gray-500 text-white hover:bg-gray-400 rounded-l-lg px-4 py-2 mx-0 outline-none focus:shadow-outline">Fund</button>
+                        <button className="bg-gray-500 text-white hover:bg-gray-400  px-4 py-2 mx-0 outline-none focus:shadow-outline">Transfer</button>
+                        <button className="bg-gray-500 text-white hover:bg-gray-400 rounded-r-lg px-4 py-2 mx-0 outline-none focus:shadow-outline">Withdraw</button>
                     </div>         
                 </div>
             </div>
