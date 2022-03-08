f39547d99b813b0d5ea4ce329280705629996e49
tijjvni <tijjvni@gmail.com>
2022-03-08 19:17:39 +0000

    chnging wallet styles

--- pages/wallet.js
+++ pages/wallet.js
@@ -27,11 +27,13 @@ export default function Home() {
                     </div>
                 </div>
 
-                <div className="flex flex-row justify-center ">
-                    <div className="flex-grow justify-center rounded-lg w-full text-sm mb-2" role="group">
-                        <button className="bg-gray-400 text-white hover:bg-gray-300 rounded-l-lg px-4 py-2 mx-0 outline-none focus:shadow-outline">Fund</button>
-                        <button className="bg-gray-400 text-white hover:bg-gray-300  px-4 py-2 mx-0 outline-none focus:shadow-outline">Transfer</button>
-                        <button className="bg-gray-400 text-white hover:bg-gray-300 rounded-r-lg px-4 py-2 mx-0 outline-none focus:shadow-outline">Withdraw</button>
+                <div className="flex flex-row">
+                    <div className="flex-grow justify-center text-center">
+                        <div className="flex rounded-lg text-sm mb-2" role="group">
+                            <button className="bg-gray-400 text-white hover:bg-gray-300 rounded-l-lg px-4 py-2 mx-0 outline-none focus:shadow-outline">Fund</button>
+                            <button className="bg-gray-400 text-white hover:bg-gray-300  px-4 py-2 mx-0 outline-none focus:shadow-outline">Transfer</button>
+                            <button className="bg-gray-400 text-white hover:bg-gray-300 rounded-r-lg px-4 py-2 mx-0 outline-none focus:shadow-outline">Withdraw</button>
+                        </div>         
                     </div>         
                 </div>
             </div>
