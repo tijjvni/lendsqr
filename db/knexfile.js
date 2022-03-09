a7dfdcdf55fdc00e4defd882d37858775de40295
tijjvni <tijjvni@gmail.com>
2022-03-09 09:38:51 +0000

    configuring node for vps

--- db/knexfile.js
+++ db/knexfile.js
@@ -9,11 +9,8 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      host : 'ec2-3-209-61-239.compute-1.amazonaws.com',
-      port : '5432',
-      database: 'de3k1k9e4f80n9',
-      user:     'rqukllqzpgvygb',
-      password: '7f9d62394009f7ad6907c3d855157289506060c6d060b5cbade646b2b6f532c6'
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
     },
     pool: {
       min: 2,
@@ -24,5 +21,19 @@ module.exports = {
       
     }
   },
+
+  production: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+      directory: __dirname + "/db/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds",
+    },
+  },  
 
 };
